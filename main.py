7281863f234027f1c84738b8fec60b58093f00e9
Yash Badgujar <128134326+MrRobot373@users.noreply.github.com>
2024-07-16 04:00:04 +0530

    Update main.py

--- main.py
+++ main.py
@@ -147,4 +147,5 @@ def recommend():
                            movie_cards=movie_cards, casts=casts, cast_details=cast_details)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
